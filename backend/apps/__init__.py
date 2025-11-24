3cf295052025286a3e06e9b563ef05097f31d72a
Nguyen Dang Khoa <146921469+khoadangnguyenn@users.noreply.github.com>
2025-11-24 18:31:25 +0700

    Update

---
+++ backend/apps/__init__.py
@@ -0,0 +1,8 @@
+"""
+Reusable Django apps that power the CNPM project.
+
+Each submodule exposes its default AppConfig so that Django can pick it up
+once the package is added to INSTALLED_APPS.
+"""
+
+
---
+++ backend/apps/roleselect/__init__.py
@@ -0,0 +1,3 @@
+default_app_config = "apps.roleselect.apps.RoleSelectConfig"
+
+
